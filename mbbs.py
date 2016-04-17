12dcb9ff9d7ee273efebdcb26caf542473779db8
Price Comstock <price.comstock@gmail.com>
2016-04-17 16:55:34 -0500

    Initial Commit

---
+++ mbbs.py
@@ -0,0 +1,30 @@
+#facebook messenger basketball simulator
+import sys
+import random
+import time
+
+#probabilities of making a shot in each set of 10 baskets
+#0-10, 11-20, 21-30, etc. in order
+probs=[.8,0.9,0.5,0.3,0.15,0.1]
+
+scores = []
+
+def scoreReport():
+	for x in range(max(scores)):
+		print str(x) + " scored " + str(scores.count(x)) + " times!"
+	print ""
+	print "High score: " + str(max(scores))
+	print "Average score: " + str(sum(scores)/float(len(scores)))
+
+while True:
+	madePrev=True
+	made=0
+	while(madePrev):
+		if random.random() < probs[made / 10]:
+			made += 1
+			print "You have made " + str(made) + " shot(s)!"
+		else:
+			print "You missed a shot after making " + str(made) + " shots!"
+			made=0
+			madePrev=False
+		time.sleep(1)
